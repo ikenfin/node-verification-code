a13b8d3facdbb030d812ed75d8048ba87d5b750f
Sergey Tominoff <ikenfin@gmail.com>
2022-03-24 17:22:14 +0400

    drop old type declarations

--- lib/types.ts
+++ lib/types.ts
@@ -1,12 +1,2 @@
 export type RandomSequenceGeneratorFn = (charCount: number) => string
 export type ChunkedCodeGeneratorFn = (charCount: number) => Buffer
-
-// export declare const createGenerator: (
-//   fn: RandomSequenceGeneratorFn
-// ) => ChunkedCodeGeneratorFn
-
-// export declare const numericSequence: RandomSequenceGeneratorFn
-
-// export declare const sequenceFromAlphabet: (
-//   alphabet: unknown[]
-// ) => RandomSequenceGeneratorFn
